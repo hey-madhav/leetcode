5940cb1a3e8038dc068f60d96c9e7eff700e048b
Madhav Sharma <madhavsharma@Madhavs-MacBook-Pro.local>
2026-05-16 09:37:36 +0530

    chores: added solution for leetcode problem 154 in golang

---
+++ 154.go
@@ -0,0 +1,19 @@
+package main
+
+func findMin2(nums []int) int {
+	left, right := 0, len(nums)-1
+
+	for left < right {
+		mid := (left + right) / 2
+
+		if nums[mid] < nums[right] {
+			right = mid
+		} else if nums[mid] > nums[right] {
+			left = mid + 1
+		} else {
+			right--
+		}
+	}
+
+	return nums[left]
+}
