eecf3e08d3f7005ca881bb40c53b344a067a8407
Madhav Sharma <madhavsharma@Madhavs-MacBook-Pro.local>
2026-05-25 22:07:45 +0530

    chore: add solution for leetcode problem 1871 in golang

---
+++ 1871.go
@@ -0,0 +1,42 @@
+package main
+
+func canReach(s string, minJump int, maxJump int) bool {
+
+    n := len(s)
+
+    queue := []int{0}
+
+    visited := make([]bool, n)
+    visited[0] = true
+
+    front := 0
+
+    far := 0
+
+    for front < len(queue) {
+
+        i := queue[front]
+        front++
+
+        if i == n-1 {
+            return true
+        }
+
+        start := max(i+minJump, far+1)
+        end := min(i+maxJump, n-1)
+
+        for j := start; j <= end; j++ {
+
+            if s[j] == '0' && !visited[j] {
+                visited[j] = true
+                queue = append(queue, j)
+            }
+        }
+
+        if end > far {
+            far = end
+        }
+    }
+
+    return false
+}
