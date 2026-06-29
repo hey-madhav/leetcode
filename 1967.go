9de4b1b7ac18144560607d5144422766e2015270
Madhav Sharma <madhavsharma@192.168.1.4>
2026-06-29 22:56:51 +0530

    chore: add solution for leetcode problem 1967 in golang

---
+++ 1967.go
@@ -0,0 +1,17 @@
+package main
+
+import "strings"
+
+func numOfStrings(patterns []string, word string) int {
+
+	count := 0
+
+	for _, pattern := range patterns {
+
+		if strings.Contains(word, pattern) {
+			count++
+		}
+	}
+
+	return count
+}
